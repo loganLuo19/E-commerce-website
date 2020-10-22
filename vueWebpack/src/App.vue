1c784955326d722ab1e70888078347be7da5c4c9
Logan <luologan19@gmail.com>
2020-10-22 14:16:40 +0800

    安裝 bootstrap/axios, 帶入 API

--- vueWebpack/src/App.vue
+++ vueWebpack/src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <div id="app">
+<div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <router-view />
+</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+    name: 'App',
+    created() {
+        // console.log(process.env.APIPATH, process.env.CUSTOMPATH);
+        const api = `${process.env.APIPATH}api/${process.env.CUSTOMPATH}/products`;
+        this.$http.get(api).then((response) => {
+            console.log(response.data)
+        })
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap"
 </style>
